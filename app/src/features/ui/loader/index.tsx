35f33799f9a146f21b280c5a6fa7c33e0b525cfc
Aejkatappaja <joverjlj@gmail.com>
2024-01-19 10:23:15 +0100

    fix(style): loader made searchbar move to the right

--- app/src/features/ui/loader/index.tsx
+++ app/src/features/ui/loader/index.tsx
@@ -4,7 +4,7 @@ import book from '@/assets/images/book.png';
 
 export const Loader: React.FC = (): JSX.Element => {
   return (
-    <div className='mx-auto flex w-screen flex-col items-center justify-center gap-8 '>
+    <div className='flex w-full flex-col items-center justify-center gap-8 '>
       <Image src={book} className='animate-spin-slow' width={150} />
       <h1 className='text-lg font-semibold text-[#404040]'>
         Chargement des données...
