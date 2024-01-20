2d540f87a9957825ad71e0b6217bd69d900925b2
Aejkatappaja <joverjlj@gmail.com>
2024-01-20 21:13:04 +0100

    fix(loader): width issue on desktop fixed

--- app/src/features/ui/loader/index.tsx
+++ app/src/features/ui/loader/index.tsx
@@ -4,7 +4,7 @@ import book from '@/assets/images/book.png';
 
 export const Loader: React.FC = (): JSX.Element => {
   return (
-    <div className='flex w-full flex-col items-center justify-center gap-8 '>
+    <div className='flex w-full flex-col items-center justify-center gap-8 border-2 md:w-screen'>
       <Image src={book} className='animate-spin-slow' width={150} />
       <h1 className='text-lg font-semibold text-[#404040]'>
         Chargement des données...
